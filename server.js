6530255205baf7bb67dc5533b3464c7f96bd84c8
Nick Carmody <ncarmody27@gmail.com>
2017-12-11 11:39:55 +1000

    Ze server

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port=3000;
+app.use(express.static('website'));
+app.listen(port,() => console.log(`Website listening on port: ${port}!`);
