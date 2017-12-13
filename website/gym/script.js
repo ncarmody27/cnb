78c4d0da32839799b5c645970b5033f43454d66b
ncarmody27 <ncarmody27@gmail.com>
2017-12-14 09:43:52 +1000

    Refactored script to make Make Table purer

--- website/gym/script.js
+++ website/gym/script.js
@@ -1,8 +1,24 @@
-let weight;
-let weightTwo;
-let weightThree;
+function setWeight(form){
+	let weight = parseFloat(form.weight.value);
+	let weightTwo = weight - 2.5;
+	let weightThree = weight - 5;
+	//squat
+	let squat = makeTable('Squat',1, weight, weightTwo, weightThree);
+	document.getElementById('squat').innerHTML = squat;
+	let bench = makeTable('Bench',1, weight, weightTwo, weightThree);
+	document.getElementById('bench').innerHTML = bench;
+	//ohp
+	var ohp = makeTable('OHP', .5, weight, weightTwo, weightThree);
+	document.getElementById('ohp').innerHTML =ohp;
+	//deadlift
+	var dl = makeTable('Deadlift', 1.25, weight, weightTwo, weightThree);
+	document.getElementById('dl').innerHTML =dl;
+	//curl
+	var crl = makeTable('Bicep Curl', .25, weight, weightTwo, weightThree);
+	document.getElementById('curl').innerHTML =crl;
+}
 
-function makeTable(exercise, modifier){
+function makeTable(exercise, modifier, weight, weightTwo, weightThree){
 	let table=`<table id=${exercise}>
 	<tr><th colspan='3'>${exercise}</th></tr>
 	<tr><th>12</th><th>10</th><th>8</th></tr>
@@ -10,23 +26,3 @@ function makeTable(exercise, modifier){
 	</table>`;
 	return table;
 }
-function setWeight(form){
-	weight = parseFloat(form.weight.value);
-	weightTwo = weight - 2.5;
-	weightThree = weight - 5;
-	
-	//squat
-	let squat = makeTable('Squat',1);
-	document.getElementById('squat').innerHTML = squat;
-	let bench = makeTable('Bench',1);
-	document.getElementById('bench').innerHTML = bench;
-	//ohp
-	var ohp = makeTable('OHP', .5);
-	document.getElementById('ohp').innerHTML =ohp;
-	//deadlift
-	var dl = makeTable('Deadlift', 1.25);
-	document.getElementById('dl').innerHTML =dl;
-	//curl
-	var crl = makeTable('Bicep Curl', .25);
-	document.getElementById('curl').innerHTML =crl;
-	}
