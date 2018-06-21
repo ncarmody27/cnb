64eec5d207284338693e3e537385e432195c6d41
ncarmody27 <ncarmody27@gmail.com>
2018-06-21 10:18:40 +1000

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static('static'))
 var port = process.env.PORT || 3000        // set our port
 var prod = 'http://cnb.technology'
 var test = 'http://localhost:3000'
-var current = test
+var current = prod
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
